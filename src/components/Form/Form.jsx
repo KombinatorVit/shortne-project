178c46820aa6276493ad21297386fb9d4213ddbc
KombinatorVit <slonik0775@gmail.com>
2023-01-13 15:01:06 +0200

    valid form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -7,11 +7,12 @@ const Form = () => {
         register,
         formState: {errors},
         handleSubmit,
-        reset,
+        // reset,
 
     } = useForm({mode: 'onSubmit'})
 
     const onSubmit = (data) => {
+        console.log(data)
     }
     return (
         <section className={classes.section}>
@@ -25,12 +26,22 @@ const Form = () => {
                         type="url"
                         placeholder="Shorten a link here..."
                         className={classes.input}
+                        {...register('Url', {
+                            required: 'Please add a link',
+                            pattern: {
+                                value: /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g,
+                                message: 'Please enter a valid url',
+                            },
+                        })}
                     />
                     <Button
                         variant="square"
                         type="submit"
                         size="medium"
                     >Shorten it!</Button>
+                    {errors.Url && (
+                        <div className={classes.error}>{errors.Url.message}</div>
+                    )}
                 </form>
             </div>
         </section>
