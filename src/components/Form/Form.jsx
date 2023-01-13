0e63b061d71758f5606ba82d2c2079458222179f
KombinatorVit <slonik0775@gmail.com>
2023-01-13 13:09:53 +0200

    work in react-hook-form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,29 +1,40 @@
 import {Button} from 'components/Button';
-
+import {useForm} from "react-hook-form";
 import classes from './Form.module.scss';
 
 const Form = () => {
-  return (
-    <section className={classes.section}>
-      <div className="container">
-        <form
-          className={classes.form}
-          autoComplete='off'
-        >
-          <input
-            type="url"
-            placeholder="Shorten a link here..."
-            className={classes.input}
-          />
-          <Button
-            variant="square"
-            type="submit"
-            size="medium"
-          >Shorten it!</Button>
-        </form>
-      </div>
-    </section>
-  )
+    const {
+        register,
+        formState: {errors},
+        handleSubmit,
+        reset,
+
+    } = useForm({mode: 'onSubmit'})
+
+    const onSubmit = (data) => {
+    }
+    return (
+        <section className={classes.section}>
+            <div className="container">
+                <form
+                    className={classes.form}
+                    autoComplete='off'
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <input
+                        type="url"
+                        placeholder="Shorten a link here..."
+                        className={classes.input}
+                    />
+                    <Button
+                        variant="square"
+                        type="submit"
+                        size="medium"
+                    >Shorten it!</Button>
+                </form>
+            </div>
+        </section>
+    )
 }
 
 export {Form};
